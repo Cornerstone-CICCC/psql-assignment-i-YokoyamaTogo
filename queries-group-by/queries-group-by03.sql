487320c4cda14fa4c4bda0747eedc6215eca7432
kirakira <15catinthebox@gmail.com>
2026-07-13 19:08:16 -0700

    queries-group-by03

--- queries-group-by/queries-group-by03.sql
+++ queries-group-by/queries-group-by03.sql
@@ -1,1 +1,8 @@
 -- Get classes more 18 students
+
+SELECT classes.name AS class_name, COUNT(students.*) AS total_students
+FROM classes
+JOIN students ON students.class_id = classes.id
+GROUP BY classes.name
+HAVING COUNT(students.*) >= 18
+ORDER BY total_students;
