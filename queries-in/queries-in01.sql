c74c44f3e0b7b8aed8d71a54a2a18aaf0867729e
kirakira <15catinthebox@gmail.com>
2026-07-13 19:23:36 -0700

    queries-in01

--- queries-in/queries-in01.sql
+++ queries-in/queries-in01.sql
@@ -1,1 +1,8 @@
 --  Get total assistance requests instructor
+
+SELECT COUNT(assistance_requests.*) AS total_assistances,
+       instructors.name AS instructor_name
+FROM assistance_requests
+JOIN instructors ON instructors.id = assistance_requests.instructor_id
+WHERE instructors.name = 'Waylon Boehm'
+GROUP BY instructors.name;
