72c4775cde924ff95a6026bec5d8537aca1a58eb
kirakira <15catinthebox@gmail.com>
2026-07-13 19:24:04 -0700

    queries-in02

--- queries-in/queries-in02.sql
+++ queries-in/queries-in02.sql
@@ -1,1 +1,8 @@
 -- Get total assistance per student
+
+SELECT COUNT(assistance_requests.*) AS total_assistances,
+       students.name AS student_name
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name;
