de5d5b2a30e54fba7d08ee5df4c1df5a62c991b9
kirakira <15catinthebox@gmail.com>
2026-07-13 19:39:26 -0700

    queries-in10

--- queries-in/queries-in10.sql
+++ queries-in/queries-in10.sql
@@ -1,1 +1,11 @@
 -- Get total assistance requests assignment
+
+SELECT assignments.id,
+       assignments.day,
+       assignments.chapter,
+       assignments.name,
+       COUNT(assistance_requests.*) AS total_assistances
+FROM assignments
+LEFT JOIN assistance_requests ON assistance_requests.assignment_id = assignments.id
+GROUP BY assignments.id, assignments.day, assignments.chapter, assignments.name
+ORDER BY total_assistances DESC;
