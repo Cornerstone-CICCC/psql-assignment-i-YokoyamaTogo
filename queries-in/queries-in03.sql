93e7bffe1d6085ad787427f9573905bd329bde01
kirakira <15catinthebox@gmail.com>
2026-07-13 19:24:47 -0700

    queries-in03

--- queries-in/queries-in03.sql
+++ queries-in/queries-in03.sql
@@ -1,1 +1,11 @@
 -- Get data assistance request
+
+SELECT instructors.name AS instructor,
+       students.name AS student,
+       assignments.name AS assignment,
+       assistance_requests.completed_at - assistance_requests.started_at AS assistance_duration
+FROM assistance_requests
+JOIN instructors ON instructors.id = assistance_requests.instructor_id
+JOIN students ON students.id = assistance_requests.student_id
+LEFT JOIN assignments ON assignments.id = assistance_requests.assignment_id
+ORDER BY assistance_duration;
