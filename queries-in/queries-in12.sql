13ecae3b8823f550ff8c5f6d4b0f0313614ccdc4
kirakira <15catinthebox@gmail.com>
2026-07-13 19:44:14 -0700

    queries-in12

--- queries-in/queries-in12.sql
+++ queries-in/queries-in12.sql
@@ -1,1 +1,10 @@
 -- Get instructor assisted class
+
+SELECT DISTINCT instructors.name AS instructor,
+                classes.name AS class
+FROM assistance_requests
+JOIN instructors ON instructors.id = assistance_requests.instructor_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN classes ON classes.id = students.class_id
+WHERE classes.name = 'JUL02'
+ORDER BY instructor;
