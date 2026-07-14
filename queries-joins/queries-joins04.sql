fbbd8f40f5ab6e8356684a1113246cd8a17f827d
kirakira <15catinthebox@gmail.com>
2026-07-13 18:52:29 -0700

    queries-joins04

--- queries-joins/queries-joins04.sql
+++ queries-joins/queries-joins04.sql
@@ -1,1 +1,7 @@
 -- Get total time assignments per student
+
+SELECT students.name AS student_name, SUM(assignment_submissions.duration) AS total_time
+FROM assignment_submissions
+JOIN students ON assignment_submissions.student_id = students.id
+WHERE students.name = 'Ibrahim Schimmel'
+GROUP BY students.name;
