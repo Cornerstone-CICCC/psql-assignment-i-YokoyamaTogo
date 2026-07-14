897bcc621db6b39e68be6223aad3a5f93a2c6494
kirakira <15catinthebox@gmail.com>
2026-07-13 19:33:51 -0700

    queries-in06

--- queries-in/queries-in06.sql
+++ queries-in/queries-in06.sql
@@ -1,1 +1,10 @@
 -- Get class longest assistance
+
+SELECT classes.name AS class,
+       AVG(assistance_requests.completed_at - assistance_requests.started_at) AS avg_duration_assistance
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN classes ON classes.id = students.class_id
+GROUP BY classes.name
+ORDER BY avg_duration_assistance DESC
+LIMIT 1;
