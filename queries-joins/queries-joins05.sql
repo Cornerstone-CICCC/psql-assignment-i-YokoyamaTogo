24a4cc2c6bc5287186ab94250b7fd45f1231c589
kirakira <15catinthebox@gmail.com>
2026-07-13 18:56:35 -0700

    queries-joins05

--- queries-joins/queries-joins05.sql
+++ queries-joins/queries-joins05.sql
@@ -1,1 +1,8 @@
 -- Get total time assignments per class
+
+SELECT classes.name AS class_name, SUM(assignment_submissions.duration) AS total_time
+FROM assignment_submissions
+JOIN students ON assignment_submissions.student_id = students.id
+JOIN classes ON students.class_id = classes.id
+WHERE classes.name = 'FEB12'
+GROUP BY classes.name;
