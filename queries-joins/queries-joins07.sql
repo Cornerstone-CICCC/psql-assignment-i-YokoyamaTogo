65069d8d53922463d16932af2bdcb1d653eeab51
kirakira <15catinthebox@gmail.com>
2026-07-13 19:01:13 -0700

    queries-joins07

--- queries-joins/queries-joins07.sql
+++ queries-joins/queries-joins07.sql
@@ -1,1 +1,8 @@
 -- Get total submissions enrolled students less 100
+
+SELECT students.name AS student, COUNT(assignment_submissions.*) AS total_submissions
+FROM assignment_submissions
+JOIN students ON assignment_submissions.student_id = students.id
+WHERE students.end_date IS NULL
+GROUP BY students.id, students.name
+HAVING COUNT(assignment_submissions.*) < 100;
