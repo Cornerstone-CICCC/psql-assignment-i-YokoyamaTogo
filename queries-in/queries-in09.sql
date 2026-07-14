0062fc6d63716fd486dedafa0f2a97be790e6754
kirakira <15catinthebox@gmail.com>
2026-07-13 19:38:33 -0700

    queries-in09

--- queries-in/queries-in09.sql
+++ queries-in/queries-in09.sql
@@ -1,1 +1,11 @@
 -- Get avg total assistance requests class
+
+SELECT AVG(total_duration) AS avg_total_duration
+FROM (
+  SELECT classes.name AS class,
+         SUM(assistance_requests.completed_at - assistance_requests.started_at) AS total_duration
+  FROM assistance_requests
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN classes ON classes.id = students.class_id
+  GROUP BY classes.name
+) AS class_totals;
