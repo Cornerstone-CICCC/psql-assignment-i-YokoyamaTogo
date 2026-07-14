1f2d9cf338bf0b1c14c61d1e20926fb0ba37cdaa
kirakira <15catinthebox@gmail.com>
2026-07-13 19:10:51 -0700

    queries-group-by04

--- queries-group-by/queries-group-by04.sql
+++ queries-group-by/queries-group-by04.sql
@@ -1,1 +1,8 @@
 -- Get total submissions per class
+
+SELECT classes.name AS class_name, COUNT(assignment_submissions.*) AS total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = assignment_submissions.student_id
+JOIN classes ON classes.id = students.class_id
+GROUP BY classes.id, classes.name
+ORDER BY total_submissions DESC;
