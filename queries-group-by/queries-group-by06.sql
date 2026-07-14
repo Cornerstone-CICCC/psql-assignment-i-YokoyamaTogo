5586faa637449530ff9f111b7bbc7acaf41b4d71
kirakira <15catinthebox@gmail.com>
2026-07-13 19:13:25 -0700

    queries-group-by06

--- queries-group-by/queries-group-by06.sql
+++ queries-group-by/queries-group-by06.sql
@@ -1,1 +1,13 @@
 -- Get enrolled students average time completion less estimated
+
+SELECT
+  students.name AS student_name,
+  AVG(assignment_submissions.duration) AS average_time,
+  AVG(assignments.duration) AS average_estimated_time
+FROM students
+JOIN assignment_submissions ON assignment_submissions.student_id = students.id
+JOIN assignments ON assignments.id = assignment_submissions.assignment_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
+ORDER BY average_time;
