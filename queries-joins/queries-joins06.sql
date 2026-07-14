0ddc6d604f086dcc054ca9423bd10db71571ba5e
kirakira <15catinthebox@gmail.com>
2026-07-13 18:59:43 -0700

    queries-joins06

--- queries-joins/queries-joins06.sql
+++ queries-joins/queries-joins06.sql
@@ -1,1 +1,7 @@
 -- Get total submissions enrolled students
+
+SELECT students.name AS student, COUNT(assignment_submissions.*) AS total_submissions
+FROM assignment_submissions
+JOIN students ON assignment_submissions.student_id = students.id
+WHERE students.end_date IS NULL
+GROUP BY students.id, students.name;
