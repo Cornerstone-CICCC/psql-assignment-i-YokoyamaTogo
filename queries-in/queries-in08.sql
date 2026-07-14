e7bf953f63449618d528f10ce70199efb9901eb3
kirakira <15catinthebox@gmail.com>
2026-07-13 19:35:36 -0700

    queries-in08

--- queries-in/queries-in08.sql
+++ queries-in/queries-in08.sql
@@ -1,1 +1,9 @@
 -- Get total assistance requests class
+
+SELECT classes.name AS class,
+       SUM(assistance_requests.completed_at - assistance_requests.started_at) AS total_duration
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN classes ON classes.id = students.class_id
+GROUP BY classes.name
+ORDER BY total_duration;
