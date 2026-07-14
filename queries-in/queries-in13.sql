3f0d09ee7a2ef36471e9265f2726769bb1ce489d
kirakira <15catinthebox@gmail.com>
2026-07-13 19:44:44 -0700

    queries-in13

--- queries-in/queries-in13.sql
+++ queries-in/queries-in13.sql
@@ -1,1 +1,12 @@
 -- Get instructors assisted class with number
+
+SELECT instructors.name AS instructor,
+       classes.name AS class,
+       COUNT(assistance_requests.*) AS total_assistances
+FROM assistance_requests
+JOIN instructors ON instructors.id = assistance_requests.instructor_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN classes ON classes.id = students.class_id
+WHERE classes.name = 'JUL02'
+GROUP BY instructors.id, instructors.name, classes.id, classes.name
+ORDER BY instructor;
