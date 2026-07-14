871190c95331df379711eb1ea5290e3656f8e9a4
kirakira <15catinthebox@gmail.com>
2026-07-13 18:04:38 -0700

    queries-joins02.sql

--- queries-joins/queries-joins02.sql
+++ queries-joins/queries-joins02.sql
@@ -1,1 +1,6 @@
 -- Get enrolled students with class name
+
+SELECT students.name AS student_name, students.email, classes.name AS class_name
+FROM students
+JOIN classes ON students.class_id = classes.id
+WHERE students.end_date IS NULL;
